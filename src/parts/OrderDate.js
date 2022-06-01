0df30298ff676015ac63c53bbddbd7382e235629
Night-Cheza <leila.kassi@outlook.com>
2022-06-01 16:07:44 -0600

    OrderDate component is created

---
+++ src/parts/OrderDate.js
@@ -0,0 +1,17 @@
+import "./OrderDate.css"
+
+function OrderDate (props) {
+    const monthDay = props.orderDate.toLocaleString("en-US", {month: "short", day: "2-digit"});
+    const year = props.orderDate.toLocaleString("en-US", {year: "numeric"});
+    const time = props.orderDate.toLocaleString("en-US", {hour12: false,  hour: "2-digit", minute: "2-digit"});
+
+    return (
+        <div className="orderDate">
+            <div className="orderDate-month">{monthDay}</div>
+            <div className="orderDate-year">{year}</div>
+            <div className="orderDate-time">{time}</div>
+        </div>
+    );
+}
+
+export default OrderDate;
--- src/parts/OrderItem.js
+++ src/parts/OrderItem.js
@@ -1,17 +1,12 @@
+import OrderDate from "./OrderDate";
 import "./OrderItem.css"
 
 //props.attribute - the attribute from app.js file tha we defined in array of objects
 function OrderItem(props) { 
-    const monthDay = props.orderDate.toLocaleString("en-US", {month: "short", day: "2-digit"});
-    const year = props.orderDate.toLocaleString("en-US", {year: "numeric"});
-    const time = props.orderDate.toLocaleString("en-US", {hour12: false,  hour: "2-digit", minute: "2-digit"});
     return (
         <div className="orderItem">
-            <div>
-                <div>{monthDay}</div>
-                <div>{year}</div>
-                <div>{time}</div>
-            </div>
+            {/* if no content between opening and closing tag, it could be written as <OrderDate />*/}
+            <OrderDate orderDate={props.orderDate}></OrderDate> 
             <div className="orderItem-name">
                 <h2>{props.item}</h2>
                 <div className="orderItem-price">${props.price}</div>
