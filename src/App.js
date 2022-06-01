09a5289ed1e53b888c2950f080f6280c9142b754
Night-Cheza <leila.kassi@outlook.com>
2022-06-01 15:17:42 -0600

    rendering date of order and name of ordered item

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
 import OrderItem from "./parts/OrderItem";
 
 function App() {
+  const orders = [
+    {
+      item: "Pizza",
+      price: 10.99,
+      orderDate: new Date(2022, 3, 15, 15, 5),
+    },
+    {
+      item: "Coca-Cola",
+      price: 2.99,
+      orderDate: new Date(2021, 5, 28, 10, 28),
+    },
+    {
+      item: "Coffee",
+      price: 3.99,
+      orderDate: new Date(2022, 2, 14, 16, 35),
+    },
+    {
+      item: "Extra Topping",
+      price: 1.99,
+      orderDate: new Date(2021, 7, 3, 12, 14),
+    }
+  ];
+
   return (
     <div>
       <h2>Starting react app</h2>
-      <OrderItem></OrderItem>
+      <OrderItem
+
+        item={orders[0].item}
+        price={orders[0].price}
+        orderDate={orders[0].orderDate}
+        orderTime={orders[0].orderTime}></OrderItem>
+      <OrderItem
+
+        item={orders[1].item}
+        price={orders[1].price}
+        orderDate={orders[1].orderDate}
+        orderTime={orders[0].orderTime}></OrderItem>
+      <OrderItem
+
+        item={orders[2].item}
+        price={orders[2].price}
+        orderDate={orders[2].orderDate}
+        orderTime={orders[0].orderTime}></OrderItem>
+      <OrderItem
+
+        item={orders[3].item}
+        price={orders[3].price}
+        orderDate={orders[3].orderDate}
+        orderTime={orders[0].orderTime}></OrderItem>
     </div>
   );
 }
--- src/parts/OrderItem.js
+++ src/parts/OrderItem.js
@@ -1,12 +1,20 @@
 import "./OrderItem.css"
 
-function OrderItem() {
+//props.attribute - the attribute from app.js file tha we defined in array of objects
+function OrderItem(props) { 
+    const monthDay = props.orderDate.toLocaleString("en-US", {month: "short", day: "2-digit"});
+    const year = props.orderDate.toLocaleString("en-US", {year: "numeric"});
+    const time = props.orderDate.toLocaleString("en-US", {hour12: false,  hour: "2-digit", minute: "2-digit"});
     return (
         <div className="orderItem">
-            <div>April 15 2022, 15:08</div>
+            <div>
+                <div>{monthDay}</div>
+                <div>{year}</div>
+                <div>{time}</div>
+            </div>
             <div className="orderItem-name">
-                <h2>Pizza</h2>
-                <div className="orderItem-price">$10.99</div>
+                <h2>{props.item}</h2>
+                <div className="orderItem-price">${props.price}</div>
             </div>
         </div>
     );
