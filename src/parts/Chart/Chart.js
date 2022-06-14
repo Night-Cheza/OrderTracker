522b5c9425cb8b83da4d5aed3b957d8d156c4e7f
Night-Cheza <leila.kassi@outlook.com>
2022-06-14 17:37:18 -0600

    Chart is created

---
+++ src/parts/Chart/Chart.js
@@ -0,0 +1,15 @@
+import ChartBars from "./ChartBars";
+import "./Chart.css";
+
+function Chart(props) {
+    return (
+        <div className="chart">
+            {/* dynamically displaying chart bars; map() is for rendering an array */}
+            {props.dataPoints.map(dataPoint => 
+                //label is unique for each bar, we can use it as a value for key
+                <ChartBars key={dataPoint.label} value={dataPoint.value} maxValue={null} label={dataPoint.label}/>)}
+        </div>
+    )
+}
+
+export default Chart;
